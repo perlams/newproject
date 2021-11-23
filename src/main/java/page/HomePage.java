5ed74e1df708c7af876b8b38320a8bce051264c5
perlams <perlams@gmail.com>
2021-11-23 17:19:44 -0600

    Adding the code to master

--- src/main/java/page/HomePage.java
+++ src/main/java/page/HomePage.java
@@ -3,7 +3,6 @@ package page;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
--- src/main/java/utils/PageFactory.java
+++ src/main/java/utils/PageFactory.java
@@ -3,13 +3,9 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class PageFactory {
 
